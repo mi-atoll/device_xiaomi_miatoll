e5a0967bf1441e951ab3459ec5717b46b94841a5
Tim Zimmermann <tim@linux4.de>
2024-05-29 10:23:48 +0200

    miatoll: Switch to AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- device.mk
+++ device.mk
@@ -52,8 +52,7 @@ $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider-service.lineage
 
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
